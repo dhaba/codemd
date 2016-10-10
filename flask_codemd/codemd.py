829e06ce4ca43bc36a4d1d60f6ac83b4c7d4c1d6
Davis Haba <dhaba@groupon.com>
2016-10-09 19:13:46 -0700

    Cloning pipeline working, but slow AF. Might have to switch to rest approach, but for now, onto viz

--- flask_codemd/codemd.py
+++ flask_codemd/codemd.py
@@ -1,12 +1,12 @@
-from flask.ext.pymongo import PyMongo
+from bson import json_util
+import logging
+
+from flask_pymongo import PyMongo
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, jsonify
 
 from repo_analyser import RepoAnalyser
 
-from bson import json_util
-import os
-import logging
 
 # Create app instance
 app = Flask(__name__)
@@ -19,13 +19,16 @@ app.config['MONGO_DBNAME'] = 'codemd'
 mongo = PyMongo(app)
 
 # Setup logging
-logger = logging.getLogger('codemd')
+log = logging.getLogger('codemd')
+log.setLevel(logging.DEBUG)
 file_handler, stream_handler = logging.FileHandler('codemd_log.log'), logging.StreamHandler()
+file_handler.setLevel(logging.DEBUG)
+stream_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s %(name)-5s %(levelname)-8s %(message)s')
 file_handler.setFormatter(formatter)
 stream_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
+log.addHandler(file_handler)
+log.addHandler(stream_handler)
 
 
 # Home page
@@ -33,41 +36,42 @@ logger.addHandler(stream_handler)
 def show_home():
     return render_template('show_home.html')
 
+
 # Main page for circle packing visualizations
 @app.route("/viz/<project_name>")
 def show_viz(project_name):
-    return render_template("viz.html", project_name = project_name)
+    return render_template("viz.html", project_name=project_name)
+
 
 # Route to fetch necessary data from github
-@app.route("/data", methods = ['POST', 'GET'])
-def data():
-    if request.method == "POST":
-        # POST from homepage, extract data if necessary and redirect to viz
-        git_url = request.form['git_url'] # TODO -- validate git url
-        project_name = git_url.split('/')[-1][0:-4]
-        repo = RepoAnalyser(project_name, mongo, working_dir=git_url)
+@app.route("/fetchdata", methods = ['POST'])
+def fetchdata():
+    # POST from homepage, extract data if necessary and redirect to viz
+    git_url = request.form['git_url'] # TODO -- validate git url
+    project_name = RepoAnalyser.short_name(git_url)
 
-        # Check if we have the data on this repo already, if not clone and extract
-        if project_name not in mongo.db.collection_names():
-            print "Data for project ", project_name, " not found. Fetching data..."
-            repo = RepoAnalyser(project_name, mongo, verbose=True, working_dir = git_url)
-            repo.persist_vcs_data()
-        else:
-            print "Data for git project ", project_name, " found."
-
-        return redirect(url_for('show_viz', project_name = project_name))
+    # Check if we have the data on this repo already, if not clone and extract
+    if project_name not in mongo.db.collection_names():
+        log.info("Data for project " + project_name + " not found. Fetching data...")
+        repo = RepoAnalyser(git_url, mongo)
+        repo.persist_commits_data()
     else:
-        # Else, return JSON for project
-        project_name = request.args.get('project_name')
-        if project_name not in mongo.db.collection_names():
-            print "Data for project: ", project_name, " not found. Go to homepage and enter git repo"
-            return None
+        log.info("Data for git project " + project_name + " found.")
 
-        # Query data from Mongo DB into json...
-        print "data found for project: ", project_name
-        commits = mongo.db[project_name].find({'revision_id': { '$exists': True}}, {'message': 0, '_id': 0})
-        data = json_util.dumps(commits, default=json_util.default)
-        return jsonify(data)
+    return "Success!" # debug line
+    return redirect(url_for('show_viz', project_name = project_name))
+    # else:
+    #     # Else, return JSON for project
+    #     project_name = request.args.get('project_name')
+    #     if project_name not in mongo.db.collection_names():
+    #         print "Data for project: ", project_name, " not found. Go to homepage and enter git repo"
+    #         return None
+    #
+    #     # Query data from Mongo DB into json...
+    #     print "data found for project: ", project_name
+    #     commits = mongo.db[project_name].find({'revision_id': { '$exists': True}}, {'message': 0, '_id': 0})
+    #     json_data = json_util.dumps(commits, default=json_util.default)
+    #     return jsonify(json_data)
 
 
 # Test route for Circle Packing Hotspot viz
--- flask_codemd/repo_analyser.py
+++ flask_codemd/repo_analyser.py
@@ -1,71 +1,109 @@
-from repository import Repository
+import logging
 import tempfile
 import sys
-from collections import defaultdict
+import os
+import shutil
+import fnmatch
 import datetime
 
 from git import Repo
+from pymongo.errors import BulkWriteError
+
+# Dictionary of hard-coded paths to include/exclude for known projects
+paths = {"scikit-learn": {"include":["sklearn/*"], "exclude":["README"]} }
 
 class RepoAnalyser(object):
-        """
-        This class extends the Repository class and provides an API for extracting
-        metrics and storing them in MongoDB for use by CodeMD's web application.
-        Unlike its parent, this class is optimized to extract all necessary metrics
-        in a single iteration of the repository's commits.
+    """
+    This class extends the Repository class and provides an API for extracting
+    metrics and storing them in MongoDB for use by CodeMD's web application.
+    Unlike its parent, this class is optimized to extract all necessary metrics
+    in a single iteration of the repository's commits.
 
-        :param working_dir: the directory of the git repository, meaning a .git
-        directory is in it (default None=pwd)
-        :param collection_name: (String) The name of the collection to store the data.
-        It should be the name of the project (i.e. scikit-learn, numpy)
-        :param mongo_instance: The instance of the flask PyMongo object where the
-        data should be written
+    :param git_url: the URL of the git repository, i.e. git://..../.git
+    :param mongo_instance: The instance of the flask PyMongo object where the
+    data should be written
+    :param include_paths:  List of file paths to be included in the extraction.
+        Ex: "*.py", "*.js", "*.css", "module1/*"
+    :param exclude_paths: List of file paths to exclude from extraction
+    :return:
+    """
+
+    def __init__(self, git_url, mongo_instance, include_paths=None, exclude_paths=None):
+        self.log = logging.getLogger('codemd.RepoAnalyser')
+        self.project_name = self.short_name(git_url)
+        self.mongo = mongo_instance.db
+        self.include_paths = include_paths
+        self.exclude_paths = exclude_paths
+
+        # Check for hard-coded paths if none specified
+        if (include_paths == None) and (exclude_paths == None) and (self.project_name in paths.keys()):
+            project_paths = paths[self.project_name]
+            self.include_paths = project_paths["include"]
+            self.exclude_paths = project_paths["exclude"]
+
+        self.log.info('cloning repository url: %s into a temporary location...', git_url)
+        self.log.debug('Included paths: %s', self.include_paths)
+        self.log.debug('Excluded paths: %s', self.exclude_paths)
+
+        self.repo_path = tempfile.mkdtemp()
+        self.repo = Repo.clone_from(git_url, self.repo_path)
+
+        self.log.info('Repository [%s] instantiated at directory: %s',
+                       self.project_name, self.repo_path)
+
+        # Get local branch name (usually "master" or "develop")
+        local_branches = list(self.repo.branches)
+        self.branch = local_branches[0].name
+
+        self.log.debug('Repository [%s] has local branches: %s\nUsing branch %s',
+                      self.project_name, local_branches, self.branch )
+
+
+    def __del__(self):
+        """
+        On delete, clean up any temporary repositories still hanging around
         :return:
         """
-
-        def __init__(self, collection_name, mongo_instance, working_dir=None, verbose=False):
-            super(RepoAnalyser, self).__init__(working_dir=working_dir, verbose=verbose)
-            self.collection_name = collection_name
-            self.mongo = mongo_instance.db
-
-            if self.verbose:
-                print "------------"
-                print "Available Collections: " + str(self.mongo.collection_names())
-                print "------------"
-
-        def fetch_vcs_data(self, branch='master'):
-            """
-            Queries the mongo collection and returns all commit entries as a list
-            of dictionaries
-
-            :return: List of dictionaries
-            """
-
-            collection = self.mongo[self.collection_name]
+        if os.path.exists(self.repo_path):
+            shutil.rmtree(self.repo_path)
 
 
-        def persist_vcs_data(self, branch='master'):
-            """
-            Scans the specified repository, and adds an entry into the
-            <repo short name> collection for each commit.
+    def persist_commits_data(self):
+        """
+        Scans the specified repository, and adds an entry into the
+        <repo short name> collection for each commit.
 
-            :return: List of dictionaries (one element for each commit)
-            """
-            if self.verbose:
-                print "Extracting metrics from git repository into collection: ", self.collection_name
+        :return: List of dictionaries (one element for each commit)
+        """
 
-            # Create collection, add creation date
-            collection = self.mongo[self.collection_name]
-            collection.insert_one({'date_updated': datetime.datetime.now(), \
-                                   'branch': branch})
+        # Define generator so commits do not have to be built in memory.
+        # This is advantageous b/c we can leverage Mongo's insert_many, which should
+        # prevent us from having to open/close a db connection repeatedly
+        def gen_commit_docs():
+            # DEBUG CODE TODO -- delete this
+            INCREMENT = 1000
+            count = 0
+            # END DEBUG
+            self.log.info("Starting iteration over commits...")
+            for c in self.repo.iter_commits(self.branch, max_count=sys.maxsize):
+                # DEBUG CODE
+                count += 1
+                if count % INCREMENT == 0:
+                    self.log.debug("# Commits processed so far: %s", count)
+                # END DEBUG
 
-            # List of dictionaries to be converted to json and added in batch
-            commits_list = []
+                # Filter ignored files. Skip commit if none of the files are relevant
+                files_included = self.__check_file_paths(c.stats.files.keys())
+                if len(files_included) == 0:
+                    # TODO -- remove debug line below
+                    # self.log.debug("# Ignoring commit [%s], no files matched inclusion\n(file names: %s)",
+                    #                 c.name_rev.split()[0], c.stats.files.keys())
+                    continue
 
-            # Enumerate over commits and a build dictionary for each
-            for c in self.repo.iter_commits(branch, max_count=sys.maxsize):
-                # Build file modifications list (list of dictionaries)
                 files_modified = []
-                for file_name, file_mods in c.stats.files.iteritems():
+                # Build file modifications list (list of dictionaries)
+                for file_name in files_included:
+                    file_mods = c.stats.files[file_name]
                     files_modified.append({'filename': file_name, \
                                          'insertions':file_mods['insertions'], \
                                          'deletions':file_mods['deletions'],
@@ -75,15 +113,66 @@ class RepoAnalyser(object):
                 revision_id = c.name_rev.split()[0]
                 commit_data = {'revision_id': revision_id, 'date': c.committed_date, \
                                'commiter': c.committer.name, 'author':c.author.name, \
-                               'message': c.message, 'files_modified': files_modified, \
-                               'branch': branch}
+                               'message': c.message, 'files_modified': files_modified}
 
-                commits_list.append(commit_data)
+                yield commit_data
 
-            if self.verbose:
-                print "Adding " + str(len(commits_list)) + " commits in batch"
+        self.log.info("Extracting metrics from git repository into db collection: %s", \
+                       self.project_name)
 
-            print type(collection)
-            collection.insert_many(commits_list)
+        collection = self.mongo[self.project_name]
+        collection.insert_one({'date_updated': datetime.datetime.now(), \
+                               'branch': self.branch})
+        collection.insert_many(doc for doc in gen_commit_docs())
+        self.log.info("Finished inserting documents into Mongo")
 
-            return commits_list
+
+    @staticmethod
+    def is_valid_git(git_url):
+        """
+        Checks if the git_url is of valid form (starts with git://.. and ends with
+        .git)
+        """
+        components = git_url.split('/')
+        return ((components[0] == 'git:') and (components[-1][-4:] == '.git'))
+
+
+    @staticmethod
+    def short_name(git_url):
+        """
+        Takes a git URL and return the short name.
+        Ex) git://github.com/scikit-learn/scikit-learn.git ==> scikit-learn
+        """
+        return git_url.split('/')[-1][0:-4]
+
+
+    def __check_file_paths(self, files):
+        """
+        Internal method to filter a list of file changes by extensions and paths.
+
+        :param files: List of filenames to filter by self.include_paths and self.exclude_paths
+        :return: List of filtered file names
+        """
+
+        if self.include_paths == None and self.exclude_paths == None:
+            return files
+
+        if self.include_paths is None or self.include_paths == []:
+            self.include_paths = ['*']
+
+        filtered_files = []
+        for f in files:
+            # count up the number of patterns in the ignore paths list that match
+            if self.exclude_paths is not None:
+                count_exclude = sum([1 if fnmatch.fnmatch(f, path) else 0 for path in self.exclude_paths])
+            else:
+                count_exclude = 0
+
+            # count up the number of patterns in the include globs list that match
+            count_include = sum([1 if fnmatch.fnmatch(f, path) else 0 for path in self.include_paths])
+
+            # if we have one vote or more to include and none to exclude, then we use the file.
+            if count_include > 0 and count_exclude == 0:
+                filtered_files.append(f)
+
+        return filtered_files
