d01c899644205bd2e88d22564bfa56bdea0adc51
Davis Haba <dhaba@groupon.com>
2016-10-16 14:41:06 -0700

    WIP on hotspots util

--- flask_codemd/metrics_builder.py
+++ flask_codemd/metrics_builder.py
@@ -3,8 +3,17 @@ import math
 import pymongo
 import fnmatch
 import logging
+from collections import defaultdict
+from itertools import combinations
+import datetime
 
 import repo_analyser
+
+
+# ::::  HEURISTICS ::::
+# Hard coded rules for inclusion/exclusion of files when calculated various metrics
+# Ideally these would be exposed to the user for custom tinkering
+LOC_THRESHOLD = 30 # min number of lines to be considered in bug score
 
 
 class MetricsBuilder(object):
@@ -16,9 +25,6 @@ class MetricsBuilder(object):
         self.collection = mongo_collection
         self.log = logging.getLogger('codemd.MetricsBuilder')
         self.regex = re.compile(r'\b(fix(es|ed)?|close(s|d)?)\b')
-
-        # minimum lines of code to be considered in hotspots analysis
-        self.LENGTH_THRESH = 10
 
 
     def commits(self):
@@ -56,19 +62,33 @@ class MetricsBuilder(object):
                          'deletions': doc['deletions'], 'total_deletions': total_deletions,
                          'author': doc['author']})
 
+        # TODO -- calculate moving average on code churn metrics
+
         return docs
 
 
-    def hotspots(self, end_date=None):
+    def hotspots(self, interval1_start=None, interval1_end=None, interval2_start=None, interval2_end=None):
         """
-        TODO -- Add docstring
+        All-in-one superhero method to calculate temporal frequency, bug score,
+        knowledge map, and code age. I should break this up but I'm way too tired
+        lol.
+
+        Acceptable invocations: Specify all parameters or specify none
+
+        All dates are assumed to be in "javascript" format.
+
+        TODO -- params, docstring, refactor, ect
         """
 
-        THRESHOLD = 20 # do not include files less than this many lines
+        # Convert times into unix epoch
+        unix_epoch = datetime.datetime(1970,1,1)).total_seconds()
+        if (interval1_start) {
+
+        }
 
         self.log.debug("Building hotspots information...")
 
-        if end_date == None:
+        if interval1_end == None:
             last_entry = list(self.collection.find(
                         {'revision_id': {'$exists': True}}).sort('date', -1))[0]
             end_date = last_entry['date']
@@ -95,7 +115,7 @@ class MetricsBuilder(object):
 
             # Check commit message for bug fixes
             if self.is_bug(mod_file['message']):
-                # just or debugging really
+                # just for debugging really
                 f['bug_dates'].append(mod_file['date'])
                 f['bug_messages'].append(mod_file['message'])
 
@@ -115,7 +135,7 @@ class MetricsBuilder(object):
         self.log.info("Highest score: %s", max_score)
 
 
-        files_to_filter = []
+        files_to_filter = [] # Files that are too short
 
         # Normalize scores
         if max_score > 0:
@@ -127,13 +147,13 @@ class MetricsBuilder(object):
                 if f_info['score'] == max_score:
                     self.log.info("Highest score was %s for file %s with info:\n%s",
                                                         max_score, f_name, f_info)
-                if f_info['loc'] < THRESHOLD:
+                if f_info['loc'] < LOC_THRESHOLD:
                     files_to_filter.append(f_name)
 
                 f_info['score'] /= max_score
 
         for f in files_to_filter:
-            self.log.debug("Removing file %s because it only had %s lines", f, files[f]['loc'])
+            # self.log.debug("Removing file %s because it only had %s lines", f, files[f]['loc'])
             files.pop(f, None)
 
         return {"name": "root", "children": self.__build_filetree(files, attributes=['score', 'loc'])}
@@ -172,6 +192,7 @@ class MetricsBuilder(object):
         keeping a pointer to the current layer, and creating a node for each
         component that does not exist yet. When it gets to the terminal node
         (bottom-most layer) it will add the attributes indicated in the attributes parameter
+        This was surprisingly nontrivial lol.
 
         TODO -- Add docstring params and returns
         """
@@ -202,12 +223,6 @@ class MetricsBuilder(object):
                         current_node = new_node['children']
 
         return tree
-
-    def is_bug(self, message):
-        """
-        DOCSTRING
-        """
-        return self.regex.match(message) != None
 
 
     def fix_fuckups(self, collection_name):
@@ -245,3 +260,46 @@ class MetricsBuilder(object):
                     print "Removing file: " + f['filename']
             if len(updated_files) != len(files_mod):
                 self.collection.update_one({'date': doc['date']}, {'$set' : {'files_modified':updated_files}})
+
+
+class HotspotsUtil(object):
+    """
+    Helper class for computing hotspot metrics (temporal coupling, code age,
+    knowledge map, bug scores), for use in Circle Packing viz.
+
+    Should only be used internally by MetricsBuilder.
+
+    :param intervals: An array of tuples, identifying the (start_time, end_time) for
+    each interval. For only one interval, pass an array with one element.
+    """
+
+
+    def __init__(self, intervals):
+        self.log = logging.getLogger('codemd.MetricsBuilder.HotspotsUtil')
+        self.regex = re.compile(r'\b(fix(es|ed)?|close(s|d)?)\b')
+
+        # Local Variables to track metrics
+        self.intervals = intervals
+        self.workingData = {}
+
+        # working data will be appended after an interval is popped
+        self.completedData = []
+
+        # convenience
+        self.max_bug_score = 0
+
+
+    def feed_file_info(file_info):
+        """
+        Accepts a file from MetricsBuilder and extracts it into various metrics
+        as needed
+        """
+
+
+
+
+    def is_bug(self, message):
+        """
+        DOCSTRING
+        """
+        return self.regex.match(message) != None
