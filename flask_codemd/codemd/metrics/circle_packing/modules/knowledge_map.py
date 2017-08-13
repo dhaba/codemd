0bb60c4ea10ded9085a05f1a74716df95733084b
Davis Haba <dhaba@groupon.com>
2017-08-12 19:38:37 -0700

    refactored knowledge map

--- flask_codemd/codemd/metrics/circle_packing/modules/knowledge_map.py
+++ flask_codemd/codemd/metrics/circle_packing/modules/knowledge_map.py
@@ -20,8 +20,6 @@ class KnowledgeMapModule(CirclePackingModule):
 
     def __init__(self, working_data, intervals):
         CirclePackingModule.__init__(self, working_data, intervals)
-        self.authors_key = {}
-        self.top_authors_count = defaultdict(int)
 
     def process_file(self, current_file):
         knowledge_info = self.get_or_create_key(current_file['filename'])
@@ -29,6 +27,8 @@ class KnowledgeMapModule(CirclePackingModule):
         knowledge_info['top_authors'][current_file['author']] += num_changes
 
     def post_process_data(self):
+        authors_key = {}
+        top_authors_count = defaultdict(int)
         # Get top few contributors for each file
         self.log.info("Starting post processing for KnowledgeMap." +
                       " Sorting modules for top authors...")
@@ -36,21 +36,21 @@ class KnowledgeMapModule(CirclePackingModule):
             knowledge_info = self.get_or_create_key(module)
             sorted_authors = sorted(knowledge_info['top_authors'].iteritems(),
                                     key = lambda (k, v): v, reverse=True)
-            self.top_authors_count[sorted_authors[0][0]] += 1
+            top_authors_count[sorted_authors[0][0]] += 1
             knowledge_info['top_authors'] = sorted_authors[0:3]
             knowledge_info['author'] = sorted_authors[0][0]
 
         self.log.info("Finished sorting modules for top authors." +
                       " Determining appropriate color map...")
-        sorted_top_authors = sorted(self.top_authors_count.iteritems(),
+        sorted_top_authors = sorted(top_authors_count.iteritems(),
                                     key = lambda (k, v): v, reverse=True)
         for i in xrange(min(len(sorted_top_authors), len(self.AUTHOR_COLORS))):
-            self.authors_key[sorted_top_authors[i][0]] = self.AUTHOR_COLORS[i]
+            authors_key[sorted_top_authors[i][0]] = self.AUTHOR_COLORS[i]
         for module in self.working_data:
             knowledge_info = self.get_or_create_key(module)
             top_author = knowledge_info['author']
-            if top_author in self.authors_key:
-                knowledge_info['color'] = self.authors_key[top_author]
+            if top_author in authors_key:
+                knowledge_info['color'] = authors_key[top_author]
             else:
                 knowledge_info['color'] = self.OTHER_COLOR
             # Convert top_authors to a dict for front end
@@ -59,9 +59,10 @@ class KnowledgeMapModule(CirclePackingModule):
             # self.log.debug("module %s with working_data: %s", module, self.working_data[module])
 
         self.log.info("Finished building author color map.")
-        # self.log.debug("Author key: %s", json.dumps(self.authors_key, indent=2))
-        # self.log.debug("Top Authors: %s", json.dumps(self.authors_key, indent=2))
+        # self.log.debug("Author key: %s", json.dumps(author_keys, indent=2))
+        # self.log.debug("Top Authors: %s", json.dumps(author_keys, indent=2))
         self.log.info("Finished post processing for KnowledgeMap.")
 
     def persist_mappings(self):
-        return {'authors_key': self.authors_key, 'top_authors_count': self.top_authors_count}
+        # All this modules data is in working_data
+        return
