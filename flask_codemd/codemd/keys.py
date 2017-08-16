03467b858df54ccd10598085223286c586ddc619
Davis Haba <davis.haba76@gmail.com>
2017-08-15 20:12:36 -0700

    removing keys

--- flask_codemd/codemd/keys.py
+++
@@ -1,7 +0,0 @@
-SECRET_KEY = "somesupersecretkeythatnoonewilleverguess"
-
-AWS_ACCESS_KEY_ID='AKIAJE64AT6LN7EW34WQ'
-AWS_SECRET_ACCESS_KEY='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'
-
-STATIC_BUCKET_NAME = 'codemd-static'
-DATA_BUCKET_NAME = 'codemd-data'
