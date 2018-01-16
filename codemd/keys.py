260f7fd41bca88cee0006c3f22a9644a50b9665b
Davis Haba <dhaba@groupon.com>
2018-01-16 13:39:55 -0800

    removed ignored files

--- codemd/keys.py
+++
@@ -1,7 +0,0 @@
-SECRET_KEY = "somesupersecretkeythatnoonewilleverguesshaha!!!!"
-
-AWS_ACCESS_KEY_ID='AKIAIW3HIY243UJQVBFA'
-AWS_SECRET_ACCESS_KEY='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'
-
-STATIC_BUCKET_NAME = 'codemd-static'
-DATA_BUCKET_NAME = 'codemd-data'
